cd6c4d3a9dcd616ea89a176fe84a0e40203dccd7
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-06 15:34:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 /*===== MENU SHOW Y HIDDEN=====*/
-const navMenu = document.getElementById
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
 //SHOW
+toggleMenu.addEventListener('click', ()=>{
 
+})
 //HIDDEN
 
 /*===== MOUSEMOVE =====*/
