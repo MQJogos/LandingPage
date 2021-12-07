f201a8b3bfdee706c17a317d4ae32937b4c4190c
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-07 16:31:22 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,4 +30,6 @@ function move(e){
 gsap.from('.nav_logo, .nav_toggle',{opacity:0, duration:1, delay: 2, y:10})
 gsap.from('.nav_item',{opacity:0, duration:1, delay: 2.1, y:30, stagger: 0.2})
 //HOME
-gsap.from('.home_title',{opacity:0, duration:1, delay: 1.6, y:30, stagger:0.2})
+gsap.from('.home_title',{opacity:0, duration:1, delay: 1.6, y:30})
+gsap.from('.home_description',{opacity:0, duration:1, delay: 1.8, y:30})
+gsap.from('.home_button',{opacity:0, duration:1, delay: 2.1, y:30})
