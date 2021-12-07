e87bb505fec36b4b021fc762ec85b26093bc3de9
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-07 16:08:45 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,13 @@ document.addEventListener('mousemove', move)
 function move(e){
     this.querySelectorAll('.move').forEach(layer =>{
          const speed = layer.getAttribute('data-speed')
+         
+         const x = (window.innerWidth - e.pageX*speed)/120
+         const x = (window.innerHeight - e.pageY*speed)/120
+    
+         layer.style.transform = 'translateX(${x}px) translateY(${y}px)'
     })
+  
 }
 /*===== GSAP ANIMATION =====*/
 
